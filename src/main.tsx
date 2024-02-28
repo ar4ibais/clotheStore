8757d5968a0de94912421496911eccaf40b1c5cd
ar4ibais <ak.hutov@yandex.ru>
2024-02-28 15:04:49 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+
+import "./scss/reset.scss";
+import "./scss/main.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
